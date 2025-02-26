68160185de92621ab05c39b9669e44852cf3ae46
ChristmasLover12345 <madnes0205@gmail.com>
2025-02-26 11:38:40 -0800

    Started on the card.

---
+++ src/Components/AdviceCardComponent.jsx
@@ -0,0 +1,12 @@
+const AdviceCardComponent = () => {
+
+
+
+    return (
+        <div className="w-[350px] md:w-[800px] h-auto bg-[323a49]">
+
+        </div>
+    )
+}
+
+export default AdviceCardComponent;
